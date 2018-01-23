a1015361d85c5be862fdae44172fe323a24ff071
johannesillenberger <johannes.illenberger@deutschebahn.com>
2018-01-23 18:31:45 +0100

    leg purpose from act type job

---
+++ core/src/main/java/de/dbanalytics/spic/job/LegPurposeFromActTypeJob.java
@@ -0,0 +1,33 @@
+package de.dbanalytics.spic.job;
+
+import de.dbanalytics.spic.data.CommonKeys;
+import de.dbanalytics.spic.data.Person;
+import de.dbanalytics.spic.data.Segment;
+import de.dbanalytics.spic.processing.SegmentTask;
+import de.dbanalytics.spic.processing.TaskRunner;
+import org.apache.commons.configuration2.HierarchicalConfiguration;
+
+import java.util.Collection;
+
+public class LegPurposeFromActTypeJob implements Job {
+
+    @Override
+    public void configure(HierarchicalConfiguration config) {
+
+    }
+
+    @Override
+    public Collection<? extends Person> execute(Collection<? extends Person> persons) {
+        TaskRunner.runLegTask(new LegPurposeFromActTypeTask(), persons);
+        return persons;
+    }
+
+    private static class LegPurposeFromActTypeTask implements SegmentTask {
+
+        @Override
+        public void apply(Segment segment) {
+            String type = segment.next().getAttribute(CommonKeys.ACTIVITY_TYPE);
+            segment.setAttribute(CommonKeys.LEG_PURPOSE, type);
+        }
+    }
+}
